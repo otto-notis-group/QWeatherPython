a94b4741f135a320c9bdc31ca5bb50d7d6f2d05e
电棍老板 <89052130+mixueicecreamandtea@users.noreply.github.com>
2023-08-29 18:52:37 +0800

    Update 首次运行程序.bat

--- 首次运行程序.bat
+++ 首次运行程序.bat
@@ -4,7 +4,7 @@ echo 运行此脚本需使用Python，如您没有安装Python，请在浏览器
 echo https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe
 echo 下载完成后安装即可。如果您不知道如何安装，请参考下面的文章。
 echo https://zhuanlan.zhihu.com/p/641659306
-echo 如果您已经拥有Python或您成功安装了Python
+echo 如果您已经拥有Python或您成功安装了Python请继续
 pause
 pip install -r requirements.txt
 rename startbatinside 启动程序.bat
