403fac5e2b1e0bf04ea8f37605148eb3855e3ed2
iambilibin <2657519792@qq.com>
2023-08-29 17:45:01 +0800

    bat

--- 启动程序.bat
+++
@@ -1,2 +0,0 @@
-echo off
-python QWeatherPython\weather.py
---
+++ 首次运行程序.bat
@@ -0,0 +1,12 @@
+echo off
+chcp 65001
+echo 运行此脚本需使用Python，如您没有安装Python，请在浏览器中输入下面的链接来下载。
+echo https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe
+echo 下载完成后安装即可。如果您不知道如何安装，请参考下面的文章。
+echo https://zhuanlan.zhihu.com/p/641659306
+echo 如果您已经拥有Python或您成功安装了Python
+pause
+pip install -r requirements.txt
+rename startbatinside 启动程序.bat
+start 启动程序.bat
+del /f /q 首次运行程序.bat
