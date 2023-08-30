71d6b13ea6bbf2081ebe49acbbbd1fca0229737c
电棍老板 <89052130+mixueicecreamandtea@users.noreply.github.com>
2023-08-30 18:27:31 +0800

    好好好

--- 首次运行程序.sh
+++ 首次运行程序.sh
@@ -1,12 +1,9 @@
-@echo off
-printf "运行此脚本需使用Python，如您没有安装Python，请在浏览器中输入下面的链接来下载。"
-printf "https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe"
-printf "下载完成后安装即可。如果您不知道如何安装，请参考下面的文章。"
-printf "https://zhuanlan.zhihu.com/p/641659306"
+printf "运行此脚本需使用Python，如您没有安装Python，请在shell中输入下面的命令来下载。"
+printf "sudo apt install python3.11"
 printf "如果您已经拥有Python或您成功安装了Python请继续"
 pause
 sudo pip install -r requirements.txt
-mv startbatinside 启动程序.command
-start 启动程序.command
+mv startbatinside 启动程序.sh
+start 启动程序.sh
 rm 首次运行程序.bat
-rm 首次运行程序.command
+rm 首次运行程序.sh
