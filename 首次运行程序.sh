5754f769e23702d6e26fca0517862b3293fb5525
电棍老板 <89052130+mixueicecreamandtea@users.noreply.github.com>
2023-08-30 22:12:28 +0800

    Add files via upload

--- 首次运行程序.sh
+++ 首次运行程序.sh
@@ -1,9 +1,6 @@
-printf "运行此脚本需使用Python，如您没有安装Python，请在shell中输入下面的命令来下载。"
-printf "sudo apt install python3.11"
-printf "如果您已经拥有Python或您成功安装了Python请继续"
-pause
+sudo apt install python3.11
 sudo pip install -r requirements.txt
 mv startbatinside 启动程序.sh
-start 启动程序.sh
+sh 启动程序.sh
 rm 首次运行程序.bat
 rm 首次运行程序.sh
