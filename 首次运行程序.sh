a15417f19f0b257d07413fc39895effaf70e3434
电棍老板 <89052130+12306ylg@users.noreply.github.com>
2024-01-06 12:38:38 +0800

    Update 首次运行程序.sh

--- 首次运行程序.sh
+++ 首次运行程序.sh
@@ -1,5 +1,5 @@
-sudo apt install python3.11
-sudo pip install -r requirements.txt
+sudo apt install python
+sudo python -m pip install -r requirements.txt
 mv startbatinside 启动程序.sh
 sh 启动程序.sh
 rm 首次运行程序.bat
